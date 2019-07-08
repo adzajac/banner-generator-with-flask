d689caa855fae6c9fcf342147d9303891972e45d
work <adrianzdev@gmail.com>
2019-07-08 23:05:04 +0100

    added options: clictag, ad serving, include tweenmax

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, redirect, url_for, request
 from jinja2 import Template
+import os
 
 app = Flask(__name__)
 
@@ -21,8 +22,15 @@ def index():
             'height':request.args.get('height', 250, type=str),
             'bkg_color':request.args.get('bkg_color', '#ffffff', type=str),
             'border':request.args.get('border', 'off', type=str),
-            'ad_server':request.args.get('ad_server', 'None', type=str)
+            'clicktag':request.args.get('clicktag', 'on', type=str),
+            'tweenmax':request.args.get('tweenmax', 'off', type=str),
+            'ad_serving':request.args.get('ad_serving', 'None', type=str)
         }
-        env.get_template('ad_base.html').stream(data=data).dump(OUTPUT_DIR+data["width"] + "x" + data["height"] + ".html")
+        dir_name = data["width"]+ "x" + data["height"]
+        if not os.path.exists(OUTPUT_DIR + dir_name):
+            os.makedirs(OUTPUT_DIR + dir_name)
+        env.get_template('ad_base.html').stream(data=data).dump(OUTPUT_DIR + dir_name + "/" + "index.html")
+        env.get_template('ad_main.js').stream(data=data).dump(OUTPUT_DIR + dir_name + "/" + "main.js")
+        env.get_template('ad_style.css').stream(data=data).dump(OUTPUT_DIR + dir_name + "/" + "style.css")
     return render_template('index.html')
 

106f25bd31300a4e9f7234a18bf169ef3a1b3ee3
work <adrianzdev@gmail.com>
2019-07-09 16:42:59 +0100

    added sample animation, border option; styling with bootstrap; added success page

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
-from flask import Flask
-from flask import render_template, redirect, url_for, request
-from jinja2 import Template
 import os
-
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for, request
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 OUTPUT_DIR = "output/"
 
-from jinja2 import Environment, PackageLoader, select_autoescape
+
+app = Flask(__name__)
+
 env = Environment(
     loader=PackageLoader('app', 'templates'),
     autoescape=select_autoescape(['html', 'xml', 'txt'])
 )
 
-
 @app.route('/')
 def index():
     if request.args:
-        data = {
-            'width':request.args.get('width', 300, type=str),
-            'height':request.args.get('height', 250, type=str),
+        ad_params = {
+            'width':request.args.get('width', 300, type=int),
+            'height':request.args.get('height', 250, type=int),
             'bkg_color':request.args.get('bkg_color', '#ffffff', type=str),
+            'border_color':request.args.get('border_color', '#525252', type=str),
             'border':request.args.get('border', 'off', type=str),
-            'clicktag':request.args.get('clicktag', 'on', type=str),
+            'clicktag':request.args.get('clicktag', 'off', type=str),
             'tweenmax':request.args.get('tweenmax', 'off', type=str),
+            'sample_animation':request.args.get('sample_animation', 'off', type=str),
             'ad_serving':request.args.get('ad_serving', 'None', type=str)
         }
-        dir_name = data["width"]+ "x" + data["height"]
-        if not os.path.exists(OUTPUT_DIR + dir_name):
-            os.makedirs(OUTPUT_DIR + dir_name)
-        env.get_template('ad_base.html').stream(data=data).dump(OUTPUT_DIR + dir_name + "/" + "index.html")
-        env.get_template('ad_main.js').stream(data=data).dump(OUTPUT_DIR + dir_name + "/" + "main.js")
-        env.get_template('ad_style.css').stream(data=data).dump(OUTPUT_DIR + dir_name + "/" + "style.css")
+        
+        dir_name = OUTPUT_DIR + str(ad_params["width"])+ "x" + str(ad_params["height"])
+        
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+            
+        env.get_template('ad_base.html').stream(ad_params=ad_params).dump(dir_name + "/index.html")
+        env.get_template('ad_main.js').stream(ad_params=ad_params).dump(dir_name + "/main.js")
+        env.get_template('ad_style.css').stream(ad_params=ad_params).dump(dir_name + "/style.css")
+        return render_template('success.html')
     return render_template('index.html')
+
 
--- templates/ad_main.js
+++ templates/ad_main.js
@@ -1,6 +1,14 @@
-// main.js
+var width = {{ad_params.width}};
+var height = {{ad_params.height}};
+{% if ad_params.tweenmax=='on' %}
+    {% if ad_params.sample_animation=='on' %}
+        // basic animation
+        TweenMax.fromTo('#text', 0.2, {opacity:0, y:-10}, {delay:0.2, opacity:1, y:0});
+        TweenMax.fromTo('#product', 0.4, {x:50}, {delay:0.4, opacity:1, x:0});
+        TweenMax.to('#text', 0.2, {delay:2, opacity:0, y:10});
+        TweenMax.to('#product', 0.2, {delay:2.0, opacity:0, x:-50});
+        TweenMax.fromTo('#logo', 0.6, {opacity:0, y:height/2-22}, {delay:2.4, opacity:1, y:height/2-12});
+    {% endif %}
+{% endif %}
 
-var width = {{data.width}};
-var height = {{data.height}};
 
-console.log("ad: " + width +"px"+ " x " + height + "px");

5f28e2e14599bbd9313fa5b440f5eff064e7d062
work <adrianzdev@gmail.com>
2019-07-10 13:13:07 +0100

    added download functionality after ad is generated

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, send_file
 from jinja2 import Environment, PackageLoader, select_autoescape
+import zipfile
+
 
 OUTPUT_DIR = "output/"
 
@@ -12,9 +14,18 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml', 'txt'])
 )
 
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        print(dirs)
+        for file in files:
+            arch_file_path = os.path.relpath(os.path.join(root,file), path)
+            ziph.write(os.path.join(root, file), arch_file_path)
+
+
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
 
@@ -33,15 +44,34 @@ def generate():
             'ad_serving':request.args.get('ad_serving', 'None', type=str)
         }
         
-        dir_name = OUTPUT_DIR + str(ad_params["width"])+ "x" + str(ad_params["height"])
+        ad_name = str(ad_params["width"])+ "x" + str(ad_params["height"])
+        ad_path = OUTPUT_DIR + ad_name
         
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
+        if not os.path.exists(ad_path):
+            os.makedirs(ad_path)
             
-        env.get_template('ad_base.html').stream(ad_params=ad_params).dump(dir_name + "/index.html")
-        env.get_template('ad_main.js').stream(ad_params=ad_params).dump(dir_name + "/main.js")
-        env.get_template('ad_style.css').stream(ad_params=ad_params).dump(dir_name + "/style.css")
+        env.get_template('ad_base.html').stream(ad_params=ad_params).dump(ad_path + "/index.html")
+        env.get_template('ad_main.js').stream(ad_params=ad_params).dump(ad_path + "/main.js")
+        env.get_template('ad_style.css').stream(ad_params=ad_params).dump(ad_path + "/style.css")
         
-        return render_template('success.html')
+        zipped_ad = zipfile.ZipFile(ad_path+'.zip', 'w', zipfile.ZIP_DEFLATED)
+        zipdir(ad_path+'/', zipped_ad)
+        zipped_ad.close()
+        
+        return redirect(url_for('success',filename=ad_name+'.zip'))
+    
     else:
         return redirect(url_for('index'))
+    
+    
+@app.route('/success/<filename>')
+def success(filename):
+    return render_template('success.html',filename=filename)
+
+
+@app.route('/download/<filename>')
+def download(filename):
+    try:
+        return send_file(OUTPUT_DIR+filename, attachment_filename=filename, as_attachment=True)
+    except Exception as e:
+        return str(e)
